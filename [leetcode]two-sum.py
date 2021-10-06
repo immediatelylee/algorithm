0ff85e0c5e9ffb69aeb02756e5e6fd1282917685
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-10-06 11:00:13 +0900

    Update [leetcode]two-sum.py

--- [leetcode]two-sum.py
+++ [leetcode]two-sum.py
@@ -40,21 +40,47 @@
 
 # 3 첫번째 수를 뺀 결과 키 조회
 
-nums = [2, 7, 11, 15]
-target = 9
-nums_map = {}
-# 키와 값을 바꿔서 딕셔너리로 저장
-for i, num in enumerate(nums):
-    nums_map[num] = i
+# nums = [2, 7, 11, 15]
+# target = 9
+# nums_map = {}
+# # 키와 값을 바꿔서 딕셔너리로 저장
+# for i, num in enumerate(nums):
+#     nums_map[num] = i
 
 # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
 # for i, num in enumerate(nums):
 #     if target - num in nums_map and i != nums_map[target - num]:
 #         print(nums.index(num), nums_map[target - num])
 
-    # return nums.index(num) , nums_map[target -num]
+# return nums.index(num) , nums_map[target -num]
 
-for i, num in enumerate(nums):
-    complement = target - num
-    if complement in nums_map and i != nums_map[complement]:
-        print(nums.index(num), nums_map[complement])
+# for i, num in enumerate(nums):
+#     complement = target - num
+#     if complement in nums_map and i != nums_map[complement]:
+#         print(nums.index(num), nums_map[complement])
+
+# from typing import List
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         nums_map = {}
+#         # 키와 값을 바꿔서 딕셔너리로 저장
+#         for i, num in enumerate(nums):
+#             nums_map[num] = i
+
+#         # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+#         for i, num in enumerate(nums):
+#             if target - num in nums_map and i != nums_map[target - num]:
+#                 return [i, nums_map[target - num]]
+
+# 4 조회구조 개선
+# from typing import List
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         nums_map = {}
+#         # 하나의 `for`문으로 통합
+#         for i, num in enumerate(nums):
+#             if target - num in nums_map:
+#                 return [nums_map[target - num], i]
+#             nums_map[num] = i
