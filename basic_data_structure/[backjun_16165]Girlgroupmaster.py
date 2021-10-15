64d3a87cc3e3aa8c4aad14122039cdc2d2301841
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-10-15 19:27:45 +0900

    Update [backjun_16165]Girlgroupmaster.py

--- basic_data_structure/[backjun_16165]Girlgroupmaster.py
+++ basic_data_structure/[backjun_16165]Girlgroupmaster.py
@@ -18,5 +18,24 @@ for i in range(problems):
     if check == 1:  # 팀이름 출력
         print(member[quest])
     else:  # 멤버 출력
-        for j in team[quest]:
+        for j in sorted(team[quest]):
             print(j)
+
+# OK
+
+# N,M = map(int,input().split())
+# team_mem,mem_team = {},{}
+# for i in range(N):
+#     team_name,mem_num = input(),int(input())
+#     team_mem[team_name]= []
+#     for j in range(mem_num):
+#         name = input()
+#         team_mem[team_name].append(name)
+#         mem_team[name]= team_name
+# for i in range(M):
+#     name,q = input(),int(input())
+#     if q:
+#         print(mem_team[name])
+#     else:
+#         for mem in sorted(team_mem[name]):
+#             print(mem)
