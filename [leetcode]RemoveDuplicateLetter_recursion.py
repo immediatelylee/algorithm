6cdae9372b0d1cc50f2e2a7953f88d2817a5963a
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-10-23 22:27:25 +0900

    211023

--- [leetcode]RemoveDuplicateLetter_recursion.py
+++ [leetcode]RemoveDuplicateLetter_recursion.py
@@ -1,19 +1,22 @@
 # https://leetcode.com/problems/remove-duplicate-letters/
 
-
-class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        # 집합으로 정렬
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):]
-            # 전체 집합과 접미사 집합이 일치할때 분리 진행
-            if set(s) == set(suffix):
-                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
-        return ''
-
-
+# 미완성  왜 ! return을 안하냐고!
 s = 'cbacdcbc'
-Solution.removeDuplicateLetters(Solution, s)
+
+
+def removeDuplicateLetters(s: str) -> str:
+    # 집합으로 정렬
+    for char in sorted(set(s)):
+        suffix = s[s.index(char):]
+        # 전체 집합과 접미사 집합이 일치할때 분리 진행
+        if set(s) == set(suffix):
+            return char + removeDuplicateLetters(suffix.replace(char, ''))
+    return ''
+
+
+removeDuplicateLetters(s)
+
+# Solution.removeDuplicateLetters(Solution, s)
 
 # class Solution:
 #     def removeDuplicateLetters(self, s: str) -> str:
