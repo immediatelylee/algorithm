915d2aa3df7baa8ea46ea4c08a4bf26dfe61f50f
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-10-25 15:14:14 +0900

    211025

--- stock/[leetcode]dailyTemperatures_answer.py
+++ stock/[leetcode]dailyTemperatures_answer.py
@@ -1,15 +1,26 @@
 from typing import List
+T = [73, 74, 75, 71, 69, 72, 76, 73]
+answer = [0] * len(T)
+stack = []
+for i, cur in enumerate(T):
+    # 현재 온도가 스택 값보다 높다면 정답 처리
+    while stack and cur > T[stack[-1]]:
+        last = stack.pop()
+        answer[last] = i - last
+    stack.append(i)
+
+print(answer)
 
 
-class Solution:
-    def dailyTemperatures(self, T: List[int]) -> List[int]:
-        answer = [0] * len(T)
-        stack = []
-        for i, cur in enumerate(T):
-            # 현재 온도가 스택 값보다 높다면 정답 처리
-            while stack and cur > T[stack[-1]]:
-                last = stack.pop()
-                answer[last] = i - last
-            stack.append(i)
+# class Solution:
+#     def dailyTemperatures(self, T: List[int]) -> List[int]:
+#         answer = [0] * len(T)
+#         stack = []
+#         for i, cur in enumerate(T):
+#             # 현재 온도가 스택 값보다 높다면 정답 처리
+#             while stack and cur > T[stack[-1]]:
+#                 last = stack.pop()
+#                 answer[last] = i - last
+#             stack.append(i)
 
-        return answer
+#         return answer
