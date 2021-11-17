f7c9e309a24364403693a0512df9d17044690e58
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-11-17 20:55:32 +0900

    211117

--- test.py
+++ test.py
@@ -1,18 +1,7 @@
-N = int(input())
-b = 0
+N = input()
 
-for i in range(1, N):
-    b = ' '*(N-i)+'*'*((2*i)-1)
-    print(b)
+count = len(input())
+while
 
-
-for i in range(N, 0, -1):
-
-    b = ' '*(N-i)+'*'*((2*i)-1)
-
-    print(b)
-
-
-# for i in range(1, N):
-#     print(''.join(answer))
-#     answer[i] = ' '
+dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+       "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
