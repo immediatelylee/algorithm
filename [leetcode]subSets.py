ea5292a216f9e838488ceccc9f9eec48671cbef5
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-11-18 18:40:42 +0900

    211118

---
+++ [leetcode]subSets.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/subsets/
+from typing import List
+
+
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+
+#         def dfs(index, path):
+#             # 매 번 결과 추가
+#             result.append(path)
+
+#             # 경로를 만들면서 DFS
+#             for i in range(index, len(nums)):
+#                 dfs(i + 1, path + [nums[i]])
+
+#         dfs(0, [])
+#         return result
+
+nums = [1, 2, 3]
+
+
+def subsets(nums: List[int]) -> List[List[int]]:
+    result = []
+
+    def dfs(index, path):
+        # 매 번 결과 추가
+        result.append(path)
+
+        # 경로를 만들면서 DFS
+        for i in range(index, len(nums)):
+            dfs(i + 1, path + [nums[i]])
+
+    dfs(0, [])
+    return result
+
+
+print(subsets(nums))
