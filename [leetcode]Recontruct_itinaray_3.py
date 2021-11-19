b1de107d73e12138f0b7c8ed0bf58b2ef21a39de
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-11-19 22:05:48 +0900

    Update [leetcode]Recontruct_itinaray_3.py

--- [leetcode]Recontruct_itinaray_3.py
+++ [leetcode]Recontruct_itinaray_3.py
@@ -2,19 +2,45 @@ import collections
 from typing import List
 
 
-class Solution:
-    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        graph = collections.defaultdict(list)
-        # 그래프 순서대로 구성
-        for a, b in sorted(tickets):
-            graph[a].append(b)
+# class Solution:
+#     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+#         graph = collections.defaultdict(list)
+#         # 그래프 순서대로 구성
+#         for a, b in sorted(tickets):
+#             graph[a].append(b)
 
-        route, stack = [], ['JFK']
-        while stack:
-            # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
-            while graph[stack[-1]]:
-                stack.append(graph[stack[-1]].pop(0))
-            route.append(stack.pop())
+#         route, stack = [], ['JFK']
+#         while stack:
+#             # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
+#             while graph[stack[-1]]:
+#                 stack.append(graph[stack[-1]].pop(0))
+#             route.append(stack.pop())
 
-        # 다시 뒤집어 어휘순 결과로
-        return route[::-1]
+#         # 다시 뒤집어 어휘순 결과로
+#         return route[::-1]
+
+# testcode
+
+# tickets = [["JFK", "SFO"], ["JFK", "ATL"], [
+#     "SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]
+tickets = [["JFK", "KUL"], ["JFK", "NRT"], ["NRT", "JFK"]]
+
+
+def findItinerary(tickets: List[List[str]]) -> List[str]:
+    graph = collections.defaultdict(list)
+    # 그래프 순서대로 구성
+    for a, b in sorted(tickets):
+        graph[a].append(b)
+
+    route, stack = [], ['JFK']
+    while stack:
+        # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
+        while graph[stack[-1]]:
+            stack.append(graph[stack[-1]].pop(0))
+        route.append(stack.pop())
+
+    # 다시 뒤집어 어휘순 결과로
+    return route[::-1]
+
+
+print(findItinerary(tickets))
