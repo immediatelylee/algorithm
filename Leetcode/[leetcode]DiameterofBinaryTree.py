97c93741a56a5e646b463877459b929a9bbcc38b
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-12-09 17:45:00 +0900

    Update [leetcode]DiameterofBinaryTree.py

--- Leetcode/[leetcode]DiameterofBinaryTree.py
+++ Leetcode/[leetcode]DiameterofBinaryTree.py
@@ -1,4 +1,17 @@
-# Definition for a binary tree node.
+# https://leetcode.com/problems/diameter-of-binary-tree/
+# case
+'''
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+'''
+#  Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
