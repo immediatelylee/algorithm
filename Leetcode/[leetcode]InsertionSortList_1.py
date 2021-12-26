f62d1319a8acae4f18f6c53c6d460cc0caaf8102
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-12-26 20:54:47 +0900

    shettle_bus

--- Leetcode/[leetcode]InsertionSortList_1.py
+++ Leetcode/[leetcode]InsertionSortList_1.py
@@ -1,10 +1,13 @@
 # https://leetcode.com/problems/insertion-sort-list/
 
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def insertionSortList(self, head: ListNode) -> ListNode:
         # 초깃값 변경
