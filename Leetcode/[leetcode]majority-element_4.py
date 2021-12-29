8046f19e91f7b9ee840ed5802f91a75b372c7523
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2021-12-29 16:44:05 +0900

    majority

---
+++ Leetcode/[leetcode]majority-element_4.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/majority-element
+from typing import List
+import collections
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        return sorted(nums)[len(nums) // 2]
