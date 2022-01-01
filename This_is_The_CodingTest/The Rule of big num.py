4838ddbb658a1baa63ea112299fc36f81ff9fb01
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-01-01 20:12:04 +0900

    The Rule of big num

--- This_is_The_CodingTest/The Rule of big num.py
+++ This_is_The_CodingTest/The Rule of big num.py
@@ -1,1 +1,17 @@
-N, M, K = int(input().split())
+N, M, K = map(int, input().split())
+
+
+nums = list(map(int, input().split()))
+nums.sort()
+counts = 0
+result = 0
+
+for i in range(M):
+    if counts != K:
+        result += nums[-1]
+        counts += 1
+    else:
+        result += nums[-2]
+        counts = 0
+
+print(result)
