9385a7d24cd9fd198229ec745a768a18664b7b8d
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-01-04 20:59:08 +0900

    Update Icing drink.py

--- This_is_The_CodingTest/Icing drink.py
+++ This_is_The_CodingTest/Icing drink.py
@@ -1,15 +1,30 @@
+import time
 N, M = map(int, input().split())
 check = [[0]*M for i in range(N)]
-drink = [list(map(int, input().split())) for i in range(N)]
+drink = [list(map(int, input())) for i in range(N)]
+
+count = 0
+start = time.time()
 
 
-def dfs(x, y):
-    if x < 0 or y < 0 or x > N or y > M:
+def dfs(x, y):  # list out of range주의
+    if x < 0 or y < 0 or x > N-1 or y > M-1:
         return False
-    if check[x, y] == 1 or drink[x][y] == 1:
+    elif check[x][y] == 1 or drink[x][y] == 1:
         return False
     check[x][y] = 1
     dfs(x-1, y)
     dfs(x+1, y)
     dfs(x, y-1)
     dfs(x, y+1)
+
+    return True
+
+
+for i in range(N):
+    for j in range(M):
+        if dfs(i, j):
+            count += 1
+
+print(count)
+print(time.time()-start)
