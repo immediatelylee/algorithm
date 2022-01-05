c5b6737a9ad7378630891f96812677f12147ca22
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-01-05 13:44:07 +0900

    Diffrentway

---
+++ Leetcode/[leetcode]Diffrent-ways-to-add-parentheses_1.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/different-ways-to-add-parentheses/
+from typing import List
+
+expression = "2*3-4*5"
+
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+
+        def compute(left, right, op):
+            results = []
+
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+
+            return results
+
+        if expression.isdigit():
+            return [int(expression)]
+
+        results = []
+
+        for index, value in enumerate(expression):
+            if value in "-+*":
+                left = self.diffWaysToCompute(expression[:index])
+                right = self.diffWaysToCompute(expression[index + 1:])
+
+                results.extend(compute(left, right, value))
+        return results
+
+
+test = Solution()
+test.diffWaysToCompute(expression)
