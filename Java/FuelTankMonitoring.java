4d1c9cd7ea7e4cbc989968772a982fd29d62731a
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-02-11 23:15:14 +0900

    Create FuelTankMonitoring.java

---
+++ Java/FuelTankMonitoring.java
@@ -0,0 +1,29 @@
+// public class FuelTankMonitoring {
+//     public void checkAndWarn()
+
+//     if (checkFuelTank()){
+//         giveWarningSignal();
+//     }
+// }
+
+// private boolean checkFuelTank(){}
+// private void giveWarningSignal(){}
+
+
+// ====================================
+
+// public class FuelTankMonitoring {
+//     public void checkAndWarn()
+// 	...
+//     if (checkFuelTank(...)){
+//         giveWarningSignal(...);
+//     }
+//     protected boolean checkFuelTank(...){...} // default방식
+//     protected boolean giveWarningSignal(...){...} // default방식
+// }
+
+// public class FuelTankMonitoringWith extends FuelTankankMonitoring{ // x방식
+// ...
+// 	protected boolean checkFuelTank(...){...} // x방식
+// 	protected boolean giveWarningSignal(...){...} // x방식
+}
