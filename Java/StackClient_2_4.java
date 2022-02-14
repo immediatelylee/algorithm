78f2cb9c9a2dc46f69ebf3c71ab6b45a54b6dd16
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-02-14 15:59:51 +0900

    delete

--- Java/StackClient_2_4.java
+++
@@ -1,8 +0,0 @@
-
-public class StackClient_2_4 {
-    public static void main(String[] args) {
-        ArrayStack_2_4 st = new ArrayStack_2_4(10);
-        st.itemArray[++st.top] = 20;
-        System.out.println(st.itemArray[st.top]);
-    }
-}
--- Java/StackClient_2_4_ArrayListUsed.java
+++
@@ -1,8 +0,0 @@
-
-public class StackClient_2_4_ArrayListUsed {
-    public static void main(String[] args) {
-        ArrayListStack_2_4 st = new ArrayListStack_2_4(10);
-        st.push(20); // st.itemArray[++st.top] = 20; -> st.push(20);
-        System.out.println(st.peek()); // System.out.println(st.itemArray[st.top]); -> System.out.println(st.peek());
-    }
-}
