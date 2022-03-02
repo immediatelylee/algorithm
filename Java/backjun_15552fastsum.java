cd82c1e5b55954398d3b8204afd13488513d7b2f
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-03-02 17:45:03 +0900

    Create backjun_15552fastsum.java

---
+++ Java/backjun_15552fastsum.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class backjun_15552fastsum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 1; i <= t; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        }
+        bw.close();
+    }
+
+}
