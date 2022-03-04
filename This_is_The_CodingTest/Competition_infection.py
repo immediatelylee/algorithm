892fc28475ed242bb3cc6879613ffbad599d007a
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-03-04 18:51:44 +0900

    Update Competition_infection.py

--- This_is_The_CodingTest/Competition_infection.py
+++ This_is_The_CodingTest/Competition_infection.py
@@ -1,41 +1,44 @@
 from collections import deque
+
 n, k = map(int, input().split())
 
-virus_map = []  # 전체 보드 정보
+graph = []  # 전체 보드 정보를 담는 리스트
 data = []  # 바이러스에 대한 정보를 담는 리스트
+
 for i in range(n):
-    virus_map.append(list(map(int, input().split())))
+    # 보드 정보를 한 줄 단위로 입력
+    graph.append(list(map(int, input().split())))
     for j in range(n):
         # 해당 위치에 바이러스가 존재하는 경우
-        if virus_map[i][j] != 0:
-            # (바이러스 종류, 시간,위치 x,위치 y) 삽입
-            data.append((virus_map[i][j], 0, i, j))
+        if graph[i][j] != 0:
+            # (바이러스 종류, 시간, 위치 X, 위치 Y) 삽입
+            data.append((graph[i][j], 0, i, j))
 
-# 정렬 이후에 큐로 옮기기(낮은 번호의 바이러스가 먼저 증식하므로)
+# 정렬 이후에 큐로 옮기기 (낮은 번호의 바이러스가 먼저 증식하므로)
 data.sort()
 q = deque(data)
 
 target_s, target_x, target_y = map(int, input().split())
 
+# 바이러스가 퍼져나갈 수 있는 4가지의 위치
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-# 바이러스는 상하좌우로 퍼짐.
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# 너비 우선 탐색 진행
+# 너비 우선 탐색(BFS) 진행
 while q:
     virus, s, x, y = q.popleft()
-    # 정확히 s초가 지나거나,큐가 빌때 까지 반복
+    # 정확히 s초가 지나거나, 큐가 빌 때까지 반복
     if s == target_s:
         break
     # 현재 노드에서 주변 4가지 위치를 각각 확인
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
+        # 해당 위치로 이동할 수 있는 경우
         if 0 <= nx and nx < n and 0 <= ny and ny < n:
             # 아직 방문하지 않은 위치라면, 그 위치에 바이러스 넣기
-            if virus[nx][ny] == 0:
-                virus_map[nx][ny] = virus
-                q.append((virus, s+1, nx, ny))
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = virus
+                q.append((virus, s + 1, nx, ny))
 
-print(virus_map[target_x - 1][target_y - 1])
+print(graph[target_x - 1][target_y - 1])
