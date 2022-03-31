0dcb60a8ba87f30cee402ae6766c18bc35291449
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-04-01 01:54:01 +0900

    Create gugudan.java

---
+++ Java/gugudan.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class gugudan {
+    public static void main(String[] args) throws IOException {
+
+        // 2단
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(2 * i);
+
+        }
+
+    }
+}
