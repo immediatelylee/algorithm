b1311460dbbc3fd5473af6e664e20091c10c3d21
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-04-09 23:36:44 +0900

    Update backjun_10872Pactorial.java

--- Java/backjun_10872Pactorial.java
+++ Java/backjun_10872Pactorial.java
@@ -5,7 +5,11 @@ import java.io.InputStreamReader;
 public class backjun_10872Pactorial {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
+        for (int i = t; i <= 0; i--) {
+
+        }
     }
 
 }
