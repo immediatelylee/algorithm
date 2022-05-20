ced25f00c2c97d3206eee520a64d62ebc1a0eb1f
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-05-21 00:00:48 +0900

    Update comprision_twoNum_1.java

--- backjun/conditional_statement(jogunmoon)_2/comprision_twoNum_1.java
+++ backjun/conditional_statement(jogunmoon)_2/comprision_twoNum_1.java
@@ -1,5 +1,5 @@
 import java.io.BufferedReader;
-import java.io.IOError;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 public class comprision_twoNum_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer = st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
