81a89b68113e3e3ae091568aae357794c10a411a
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-06-08 02:13:33 +0900

    hind&seek

--- fastcampus_CodingTest/TC_6_Graph/a_2_[backjun_1697]hide&seek.py
+++ fastcampus_CodingTest/TC_6_Graph/a_2_[backjun_1697]hide&seek.py
@@ -1,9 +1,12 @@
 # https://www.acmicpc.net/problem/1697
 from collections import deque
+from timeit import default_timer as timer
+from datetime import timedelta
 
 
 MAX = 100001
 n, k = map(int, input().split())
+start = timer()
 array = [0] * MAX
 
 
@@ -14,9 +17,15 @@ def bfs():
         if now_pos == k:
             return array[now_pos]
         for next_pos in (now_pos - 1, now_pos + 1, now_pos * 2):
+            # 한번도 안가고 범위 벗어나지 않으면
             if 0 <= next_pos < MAX and not array[next_pos]:
                 array[next_pos] = array[now_pos] + 1
                 q.append(next_pos)
 
 
 print(bfs())
+
+end = timer()
+
+print("Time elapsed: ", end - start)  # seconds
+print("Time elapsed: ", timedelta(seconds=end-start))
--- snippet/timecheck.py
+++ snippet/timecheck.py
@@ -1,14 +1,14 @@
-import time
+from timeit import default_timer as timer
 from datetime import timedelta
 
-start = time.process_time()
+start = timer()
 
 # // 구현부
 # sum = 0
 # for i in range(10000000):
 #     sum += i
 
-end = time.process_time()
+end = timer()
 
 print("Time elapsed: ", end - start)  # seconds
 print("Time elapsed: ", timedelta(seconds=end-start))
