93734261456819d133176217ae846baf8ee9b07e
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-06-19 00:41:09 +0900

    Update b_1_[backjun_2606]virus_IO.py

--- fastcampus_CodingTest/TC_6_Graph/b_1_[backjun_2606]virus_IO.py
+++ fastcampus_CodingTest/TC_6_Graph/b_1_[backjun_2606]virus_IO.py
@@ -1,17 +1,47 @@
 # https://www.acmicpc.net/problem/2606
 # pc수가 적으므로 dfs가 간편하다.
-# 시작하는 정점은 빼야하므로 1을뺀다. 
+# 시작하는 정점은 빼야하므로 1을뺀다.
+
+from collections import deque
+import sys
+
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+
+# inputdata
+# 7
+# 6
+# 1 2
+# 2 3
+# 1 5
+# 5 2
+# 5 6
+# 4 7
+# -> 4
 
 n = int(input())
 c = int(input())
+count = 0
 adj = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
-count = 0
 
 for _ in range(c):
     x, y = map(int, input().split())
     adj[x].append(y)
     adj[y].append(x)
+
+
+def bfs(v):
+    global count
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if not(visited[v]):
+            visited[v] = True
+            count += 1
+            for e in adj[v]:
+                if not visited[e]:
+                    q.append(e)
 
 
 def dfs(now_pos):
@@ -23,5 +53,6 @@ def dfs(now_pos):
             dfs(next_pos)
 
 
-dfs(1)
+bfs(1)
+# dfs(1)
 print(count - 1)
