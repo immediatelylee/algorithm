e76113f03e2a6f484581674e9be57e54d8283622
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-06-20 15:28:31 +0900

    Update LuckyStrait_me.py

--- This_is_The_CodingTest/LuckyStrait_me.py
+++ This_is_The_CodingTest/LuckyStrait_me.py
@@ -3,7 +3,7 @@ n = list(map(int, input()))  # 스니핏에 넣어놓기
 middle = (len(n) // 2)
 
 
-a = sum(n[:middle])  # 왜 3이 나오지?
+a = sum(n[:middle])  # 왜 3이 나오지? [:3] = 0,1,2
 b = sum(n[middle:])
 if a == b:
     print('LUCKY')
