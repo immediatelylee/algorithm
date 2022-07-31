4a976611a199860f40a63793855e8295a838ae25
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-07-31 20:47:32 +0900

    Create p6-1_example_selevtive_sort.py

---
+++ This_is_The_CodingTest/[6]Sort/p6-1_example_selevtive_sort.py
@@ -0,0 +1,15 @@
+'''
+선택정렬
+가장 낮은 데이터를 선택해 맨 앞에 데이터와 바꾸고 
+그다음 작은 데이터를 선택헤 앞에서 두번째 데이터와 바꾸는과정
+'''
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[j] = array[j], array[i]
+
+print(array)
