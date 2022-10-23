20c49b4dd225fe137d2145cce07f4dd755ab1590
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2022-10-23 19:02:16 +0900

    Update drinking_beer_and_walking_9205.py

--- backjun/backjun_basic_DFS_BFS/drinking_beer_and_walking_9205.py
+++ backjun/backjun_basic_DFS_BFS/drinking_beer_and_walking_9205.py
@@ -2,27 +2,33 @@
 
 from collections import deque
 
+# 1000 거리 안에 있으면 while문을 게속 돌림 
+# nx,ny와 x,y의 거리 차가 1000 이내라면 방문하고 q에 넣고
+def bfs():
+    q= deque()
+    q.append([home[0],home[1]])
+    while q:
+        x,y =q.popleft()
+        if abs(x - festival[0]) + abs(y-festival[1]) <= 1000:
+            print("happy")
+            return
+        for i in range(n):
+            if not visited[i]:
+                nx,ny = cs[i]
+                if abs(x - nx) + abs(y - ny)<= 1000:
+                    q.append([nx,ny])
+                    visited[i] =1
+    print("sad")
+    return
+
 t = int(input())
-
-
-def bfs(x, y):
-    pass
-
-    print()
-
-
 for i in range(t):
-    n = int(input)
+    n = int(input())
+    home =[int(x) for x in input().split()]
     cs = []
-    a, b = map(int, input().split())  # start
-
     for j in range(n):
-        x, y = map(int, input().split())
-        cs.append((x, y))
-
-    bfs(a, b)
-
-
-# for i in range(t):
-
-# n = int(input())
+        x,y = map(int,input().split())
+        cs.append([x,y])
+    festival = [int(x) for x in input().split()]
+    visited = [0 for i in range(n+1)]
+    bfs()
