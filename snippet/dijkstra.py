901d27c3af9a00d6339b197ac6df619868845e4a
commit name <dydgh1095@naver.com>
2022-11-23 13:01:58 +0900

    Update dijkstra.py

--- snippet/dijkstra.py
+++ snippet/dijkstra.py
@@ -25,6 +25,7 @@ for _ in range(m):
 def dijkstra(start):
     q = []
     # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    # 여기서 cost,index순으로 넣는다 최소힙을 쓸거기 때문에
     heapq.heappush(q, (0, start))
     # heappush (입력할 대상리스트,추가할원소)
     distance[start] = 0
@@ -35,12 +36,13 @@ def dijkstra(start):
         if distance[now] < dist:
             continue
         # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
-            cost = dist + i[1]
+        for node_index, node_cost in graph[now]:
+            cost = dist + node_cost
             # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+            if cost < distance[node_index]:
+                distance[node_index] = cost
+                # 최소 힙을 써야 하기 때문에 cost,index순으로 넣는다.
+                heapq.heappush(q, (cost, node_index))
 
 
 # 다익스트라 알고리즘을 수행
