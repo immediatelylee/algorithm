eee583ec19825188c9f9ff0e291091190b5d98a9
commit name <dydgh1095@naver.com>
2022-11-28 03:58:50 +0900

    Create p7-5_practice부품찾기.py

---
+++ This_is_The_CodingTest/[7]binary_search/p7-5_practice부품찾기.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = list(map(int, input().split()))
+
+target_number = int(input())
+target_list = list(map(int, input().split()))
+
+
+def binary_search(array, target, start, end):
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == target:
+            return target
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+result = []
+
+for target in target_list:
+    if binary_search(array, target, 0, n-1) == target:
+        result.append("yes")
+    else:
+        result.append("no")
+
+print(result)
