70256eaa15b0ddf135d79fcb0a49e426d72a32c7
commit name <dydgh1095@naver.com>
2022-11-29 23:26:34 +0900

    Create p8_3_practice_ant_worrior.py

---
+++ This_is_The_CodingTest/[8]DP/p8_3_practice_ant_worrior.py
@@ -0,0 +1,15 @@
+'''
+ai = max(ai-1,ai-2+k)
+'''
+
+n = int(input())
+dp = [0]*100
+b_store = list(map(int, input().split()))
+
+dp[0] = b_store[0]
+dp[1] = max(b_store[0], b_store[1])
+
+for i in range(2, len(b_store)):
+    dp[i] = max(dp[i-1], dp[i-2]+b_store[i])
+
+print(dp[n-1])
