0efa41256c8e6d4652f3a117d0b30bb1babbc012
commit name <dydgh1095@naver.com>
2022-12-21 22:11:07 +0900

    Create [02]곱하기 혹은 더하기.py

---
+++ This_is_The_CodingTest/practice_problem_set/[p]01Greedy/[02]곱하기 혹은 더하기.py
@@ -0,0 +1,12 @@
+data = list(map(int, input()))
+
+result = data[0]
+
+
+for i in range(1, len(data)):
+    if data[i-1] <= 1 or data[i] <= 1:
+        result += data[i]
+    else:
+        result *= data[i]
+
+print(result)
