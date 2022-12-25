a8f62bac9fe7806b3c9fcdd785413e0b82f943e3
commit name <dydgh1095@naver.com>
2022-12-25 22:31:56 +0900

    Update [04]만들 수 없는 금액.py

--- This_is_The_CodingTest/practice_problem_set/[p]01Greedy/[04]만들 수 없는 금액.py
+++ This_is_The_CodingTest/practice_problem_set/[p]01Greedy/[04]만들 수 없는 금액.py
@@ -1,4 +1,13 @@
 n = int(input())
-coin_type = list(map(int(input())))
+coin_type = list(map(int(input().split())))
 coin_type.sort()
-making_number_check = [0] * 1000001
+
+target = 1
+for x in coin_type:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < x:
+        break
+    target += x
+
+# 만들 수 없는 금액 출력
+print(target)
