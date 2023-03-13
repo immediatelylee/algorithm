deff81a97ab1e712304040053175cc6c03355d9e
commit name <dydgh1095@naver.com>
2023-03-13 13:47:07 +0900

    Create [27]정렬된 배열에서 특정 수의 개수 구하기.py

---
+++ This_is_The_CodingTest/practice_problem_set/[p]Binary Search/[27]정렬된 배열에서 특정 수의 개수 구하기.py
@@ -0,0 +1,15 @@
+import bisect
+
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
+
+# bisect_left(a, x): 정렬된 순서를 유지하면서 배열 a에 x를 삽입할 가장 왼쪽 인덱스를 반환
+left = bisect.bisect_left(array, x)
+
+# bisect_right(a, x): 정렬된 순서를 유지하면서 배열 a에 x를 삽입할 가장 오른쪽 인덱스를 반환
+right = bisect.bisect_right(array, x)
+
+if right - left == 0:
+    print(-1)
+else:
+    print(right - left)
